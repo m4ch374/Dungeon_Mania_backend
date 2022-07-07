95ddfa6b7cbf74a1fa0339d6dd2efd6c70a3281e
Henry Wan <z5338828@ad.unsw.edu.au>
2022-07-08 01:51:48 +1000

    Setup: Completed a list of entity types

---
+++ src/main/java/dungeonmania/DungeonObjects/EntityTypes.java
@@ -0,0 +1,36 @@
+package dungeonmania.DungeonObjects;
+
+// A list of types
+public class EntityTypes {
+
+    // Player
+    public static final String PLAYER               = "player";
+
+    // Static Entities
+    public static final String WALL                 = "wall";
+    public static final String EXIT                 = "exit";
+    public static final String BOULDER              = "boulder";
+    public static final String FLOOR_SWITCH         = "switch";
+    public static final String DOOR                 = "door";
+    public static final String PORTAL               = "portal";
+    public static final String ZOMBIE_TOAST_SPAWNER = "zombie_toast_spawner";
+
+    // Movables
+    public static final String SPIDER               = "spider";
+    public static final String ZOMBIE_TOAST         = "zombie_toast";
+    public static final String MERCENARY            = "mercenary";
+
+    // Collectables
+    public static final String TREASURE             = "treasure";
+    public static final String KEY                  = "key";
+    public static final String INVINCIBILITY_POTION = "invincibility_potion";
+    public static final String INVISIBILITY_POTION  = "invisibility_potion";
+    public static final String WOOD                 = "wood";
+    public static final String ARROWS               = "arrow";
+    public static final String BOMB                 = "bomb";
+    public static final String SWORD                = "sword";
+
+    // Craftables
+    public static final String BOW                  = "bow";
+    public static final String SHIELD               = "shield";
+}
