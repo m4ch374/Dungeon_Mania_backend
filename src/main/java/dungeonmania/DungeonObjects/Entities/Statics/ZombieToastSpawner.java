0f32189304ecedec8567e890f16b82f5786a2186
Henry Wan <henry.wan@student.unsw.edu.au>
2022-07-15 21:21:05 +1000

    Development: Finished zombie toast spawning behaviours

--- src/main/java/dungeonmania/DungeonObjects/Entities/Statics/ZombieToastSpawner.java
+++ src/main/java/dungeonmania/DungeonObjects/Entities/Statics/ZombieToastSpawner.java
@@ -1,28 +1,94 @@
 package dungeonmania.DungeonObjects.Entities.Statics;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 import org.json.JSONObject;
 
+import dungeonmania.DungeonObjects.EntityTypes;
+import dungeonmania.DungeonObjects.DungeonMap.DungeonMap;
 import dungeonmania.DungeonObjects.Entities.Entity;
+import dungeonmania.DungeonObjects.Entities.Characters.ZombieToast;
 import dungeonmania.Interfaces.ISpawnable;
 import dungeonmania.Interfaces.IStaticInteractable;
 import dungeonmania.response.models.EntityResponse;
+import dungeonmania.util.Direction;
+import dungeonmania.util.Position;
 import dungeonmania.util.DungeonFactory.EntityStruct;
 
 public class ZombieToastSpawner extends Entity implements IStaticInteractable, ISpawnable {
-    int zombieSpawnRate;
+    private static int spawnId = 0;
+
+    private int zombieSpawnRate;
+    private DungeonMap map;
 
     public ZombieToastSpawner(EntityStruct metaData, JSONObject config) {
         super(metaData);
         this.zombieSpawnRate = config.getInt("zombie_spawn_rate");
+        map = super.getMap();
     }
 
     @Override
-    public EntityResponse toEntityResponse() { return null; }
+    public EntityResponse toEntityResponse() {
+        return new EntityResponse(super.getId(), super.getType(), map.getEntityPos(this), true);
+    }
 
     @Override
-    public void spawn() {}
+    public void spawn(JSONObject config, int currTick) {
+        if (zombieSpawnRate == 0)
+            return;
+
+        if (currTick == 0 || currTick % zombieSpawnRate != 0)
+            return;
+
+        Position currPos = map.getEntityPos(this);
+        List<Position> possiblePos = getPotentialSpawnPos(currPos);
+
+        if (possiblePos.isEmpty()) 
+            return;
+
+        Random random = new Random();
+        int randIdx = random.nextInt(possiblePos.size());
+        Position newPos = possiblePos.get(randIdx);
+
+        EntityStruct struct = new EntityStruct("spawned_zombie" + spawnId, EntityTypes.ZOMBIE_TOAST.toString(), map);
+        ZombieToast zombieSpawned = new ZombieToast(struct, config);
+        map.placeEntityAt(zombieSpawned, newPos);
+
+        spawnId++;
+    }
 
     @Override
     public void interactedBy(Entity interactor) {}
     
+
+    private List<Position> getPotentialSpawnPos(Position currPos) {
+        List<Position> possiblePos = new ArrayList<Position>();
+
+        for (Direction d : Direction.values()) {
+            Position dPos = currPos.translateBy(d);
+
+            if (!posContainBlockable(dPos))
+                possiblePos.add(dPos);
+        }
+        return possiblePos;
+    }
+
+    private boolean posContainBlockable(Position pos) {
+        List<Entity> entities = map.getEntitiesAt(pos);
+
+        for (Entity e : entities) {
+            if (e.getType().equals(EntityTypes.WALL.toString()))
+                return true;
+
+            if (e.getType().equals(EntityTypes.DOOR.toString())) {
+                Door d = (Door) e;
+                if (!d.isOpen())
+                    return true;
+            }
+        }
+
+        return false;
+    }
 }
--- src/test/java/dungeonmania/SpawningTests.java
+++ src/test/java/dungeonmania/SpawningTests.java
@@ -1,5 +1,6 @@
 package dungeonmania;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.DisplayName;
@@ -7,6 +8,7 @@ import org.junit.jupiter.api.Test;
 
 import dungeonmania.response.models.DungeonResponse;
 import dungeonmania.util.Direction;
+import dungeonmania.util.Position;
 
 public class SpawningTests {
     private static final String D_DIR_NAME = "d_SpawnTests/";
@@ -65,6 +67,104 @@ public class SpawningTests {
         for (int i = 0; i < 100; i++) {
             res = dmc.tick(Direction.UP);
             assertTrue(TestUtils.getEntities(res, "spider").size() == 0);
+        }
+    }
+
+    @Test
+    @DisplayName("Test zombie toast spawner spawns zombies for each tick")
+    public void testZombieToasSpawnerSpawns_spawnsEachTick() {
+        DungeonManiaController dmc = new DungeonManiaController();
+        DungeonResponse res = dmc.newGame(D_DIR_NAME + "d_spawnTest_zombieSpawnBasic", C_DIR_NAME + "c_spawnTests_zombieSpawnEachTick");
+
+        assertTrue(TestUtils.getEntities(res, "zombie_toast").size() == 0);
+
+        for (int i = 0; i < 100; i++) {
+            res = dmc.tick(Direction.UP);
+            assertTrue(TestUtils.getEntities(res, "zombie_toast").size() == i + 1);
+        }
+    }
+
+    @Test
+    @DisplayName("Test zombie toast spawner spawns zombies for every 2 ticks")
+    public void testZombieToasSpawnerSpawns_spawnsEvery2Ticks() {
+        DungeonManiaController dmc = new DungeonManiaController();
+        DungeonResponse res = dmc.newGame(D_DIR_NAME + "d_spawnTest_zombieSpawnBasic", C_DIR_NAME + "c_spawnTests_zombieSpawnEvery2Ticks");
+
+        assertTrue(TestUtils.getEntities(res, "zombie_toast").size() == 0);
+
+        for (int i = 0; i < 100; i++) {
+            res = dmc.tick(Direction.UP);
+            assertTrue(TestUtils.getEntities(res, "zombie_toast").size() == (int)((i + 1) / 2));
+        }
+    }
+
+    @Test
+    @DisplayName("Test zombie toast spawner spawns zombies for every 7 ticks")
+    public void testZombieToasSpawnerSpawns_spawnsEvery7Ticks() {
+        DungeonManiaController dmc = new DungeonManiaController();
+        DungeonResponse res = dmc.newGame(D_DIR_NAME + "d_spawnTest_zombieSpawnBasic", C_DIR_NAME + "c_spawnTests_zombieSpawnEvery7Ticks");
+
+        assertTrue(TestUtils.getEntities(res, "zombie_toast").size() == 0);
+
+        for (int i = 0; i < 100; i++) {
+            res = dmc.tick(Direction.UP);
+            assertTrue(TestUtils.getEntities(res, "zombie_toast").size() == (int)((i + 1) / 7));
+        }
+    }
+
+    @Test
+    @DisplayName("Test zombie does not spawn")
+    public void testZombieToastDoesNotSpawn() {
+        DungeonManiaController dmc = new DungeonManiaController();
+        DungeonResponse res = dmc.newGame(D_DIR_NAME + "d_spawnTest_zombieSpawnBasic", "c_spiderTest_basicMovement");
+
+        assertTrue(TestUtils.getEntities(res, "zombie_toast").size() == 0);
+
+        for (int i = 0; i < 100; i++) {
+            res = dmc.tick(Direction.UP);
+            assertTrue(TestUtils.getEntities(res, "zombie_toast").size() == 0);
+        }
+    }
+
+    @Test
+    @DisplayName("Test zombie toast spawner does not spawn if it is blocked")
+    public void testZombieToastDoesNotSpawn_spawnerBlockedByWalls() {
+        DungeonManiaController dmc = new DungeonManiaController();
+        DungeonResponse res = dmc.newGame(D_DIR_NAME + "d_spawnTest_zombieSpawnerBlocked", C_DIR_NAME + "c_spawnTests_zombieSpawnEachTick");
+
+        assertTrue(TestUtils.getEntities(res, "zombie_toast").size() == 0);
+
+        for (int i = 0; i < 100; i++) {
+            res = dmc.tick(Direction.UP);
+            assertTrue(TestUtils.getEntities(res, "zombie_toast").size() == 0);
+        }
+    }
+
+    @Test
+    @DisplayName("Test zombie toast spawner does not spawn if it is blocked")
+    public void testZombieToastDoesNotSpawn_spawnerBlockedByDoors() {
+        DungeonManiaController dmc = new DungeonManiaController();
+        DungeonResponse res = dmc.newGame(D_DIR_NAME + "d_spawnTest_zombieSpawnerBlockedByDoors", C_DIR_NAME + "c_spawnTests_zombieSpawnEachTick");
+
+        assertTrue(TestUtils.getEntities(res, "zombie_toast").size() == 0);
+
+        for (int i = 0; i < 100; i++) {
+            res = dmc.tick(Direction.UP);
+            assertTrue(TestUtils.getEntities(res, "zombie_toast").size() == 0);
+        }
+    }
+
+    @Test
+    @DisplayName("Test zombie toast spawner spawns in one direction")
+    public void testZombieToastSpawnsInOneDirection() {
+        DungeonManiaController dmc = new DungeonManiaController();
+        DungeonResponse res = dmc.newGame(D_DIR_NAME + "d_spawnTest_zombieSpawnerOneOpening", C_DIR_NAME + "c_spawnTests_zombieSpawnEachTick");
+
+        assertTrue(TestUtils.getEntities(res, "zombie_toast").size() == 0);
+
+        for (int i = 0; i < 100; i++) {
+            res = dmc.tick(Direction.UP);
+            assertEquals(new Position(3, 3), TestUtils.getEntityById(res, "spawned_zombie" + i).getPosition());
         }
     }
 }
