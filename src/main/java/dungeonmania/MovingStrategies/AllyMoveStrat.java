7d3b074a867e67abc099ea8439e4e919d2d5608f
Henry Wan <henry.wan@student.unsw.edu.au>
2022-07-16 16:09:00 +1000

    Refactor: ally move strat does not require player state

--- src/main/java/dungeonmania/MovingStrategies/AllyMoveStrat.java
+++ src/main/java/dungeonmania/MovingStrategies/AllyMoveStrat.java
@@ -12,30 +12,33 @@ import dungeonmania.util.Position;
 public class AllyMoveStrat implements IMovingStrategy {
 
     private Entity mover;
-    private Player following = null;
+    private Player following;
+    private Position followingPrevPos;
 
     private DungeonMap map;
     
     public AllyMoveStrat(Entity mover, DungeonMap map) {
         this.mover = mover;
         this.map = map;
+
+        following = map.getAllEntities().stream()
+                    .filter(e -> e.getType().equals(EntityTypes.PLAYER.toString()))
+                    .map(e -> (Player) e)
+                    .findFirst()
+                    .get();
+
+        followingPrevPos = map.getEntityPos(following);
     }
 
     @Override
     public void moveEntity() {
-        if (following == null)
-            following = map.getAllEntities().stream()
-                        .filter(e -> e.getType().equals(EntityTypes.PLAYER.toString()))
-                        .map(e -> (Player) e)
-                        .findFirst()
-                        .get();
-
-        Position followingCurrPos = (Position) following.getState().get("currentPosition");
-        Position followingPrevPos = (Position) following.getState().get("previousPosition");
+        Position followingCurrPos = map.getEntityPos(following);
 
         // Move only if ally does not overlap with player
-        if (!followingCurrPos.equals(followingPrevPos))
+        if (!followingCurrPos.equals(followingPrevPos)) {
             map.moveEntityTo(mover, followingPrevPos);
+            followingPrevPos = followingCurrPos;
+        }
     }
     
 }
