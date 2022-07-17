4e329ae216be5cec691a2d2a55c4bba856281531
HANQIONE <hanqibai416@gmail.com>
2022-07-17 22:21:15 +1000

    boulder-switch-bomb interaction

--- src/main/java/dungeonmania/DungeonObjects/Entities/LogicEntities/Statics/FloorSwitch.java
+++ src/main/java/dungeonmania/DungeonObjects/Entities/LogicEntities/Statics/FloorSwitch.java
@@ -1,8 +1,14 @@
 package dungeonmania.DungeonObjects.Entities.LogicEntities.Statics;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 import dungeonmania.DungeonObjects.Entities.Entity;
 import dungeonmania.DungeonObjects.Entities.Statics.Boulder;
 import dungeonmania.Interfaces.IStaticInteractable;
+import dungeonmania.util.Direction;
 import dungeonmania.util.Position;
 import dungeonmania.util.DungeonFactory.EntityStruct;
 import dungeonmania.exceptions.*;
@@ -21,6 +27,23 @@ public class FloorSwitch extends Entity implements IStaticInteractable {
                                             .anyMatch(e -> (e instanceof Boulder));
 
         return isActive;
+    }
+
+    public void active() {
+        Position pos = getMap().getEntityPos(this);
+
+        ArrayList<Entity> enties = new ArrayList<Entity>();
+
+        enties.addAll(getMap().getEntitiesAt(pos.translateBy(Direction.UP)));
+        enties.addAll(getMap().getEntitiesAt(pos.translateBy(Direction.DOWN)));
+        enties.addAll(getMap().getEntitiesAt(pos.translateBy(Direction.LEFT)));
+        enties.addAll(getMap().getEntitiesAt(pos.translateBy(Direction.RIGHT)));
+
+        enties
+            .stream()
+            .filter(e -> (e instanceof Bomb))
+            .map(e -> (Bomb) e)
+            .forEach(e -> e.activate(getMap().getEntityPos(e)));
     }
 
     @Override
