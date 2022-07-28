63aa16037ea73ff31e0fb1c4e69f117596f56ce1
Henry Wan <z5338828@ad.unsw.edu.au>
2022-07-29 05:19:45 +1000

    Test: written more tests

---
+++ src/test/java/dungeonmania/AssassinBehaviourTest.java
@@ -0,0 +1,56 @@
+package dungeonmania;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import dungeonmania.response.models.DungeonResponse;
+import dungeonmania.util.Direction;
+import dungeonmania.util.Position;
+
+public class AssassinBehaviourTest {
+    private static final String DIR_PATH = "d_AssassinTests/";
+
+    @Test
+    @DisplayName("Test Assassin move towards player basic")
+    public void testMovement_basic() {
+        DungeonManiaController dmc = new DungeonManiaController();
+
+        DungeonResponse res = dmc.newGame(DIR_PATH + "d_assassinTest_basicMovement", "c_msic_zeroDamage");
+
+        Position mercPos = TestUtils.getEntityById(res, "assassin").getPosition();
+        assertEquals(new Position(1, 5), mercPos);
+
+        res = dmc.tick(Direction.UP);
+
+        mercPos = TestUtils.getEntityById(res, "assassin").getPosition();
+        assertEquals(new Position(1, 4), mercPos);
+
+        res = dmc.tick(Direction.UP);
+
+        mercPos = TestUtils.getEntityById(res, "assassin").getPosition();
+        assertEquals(new Position(1, 3), mercPos);
+    }
+
+    @Test
+    @DisplayName("Test Assassin does not flee")
+    public void testMovement_doesNotFlee() {
+        DungeonManiaController dmc = new DungeonManiaController();
+
+        DungeonResponse res = dmc.newGame(DIR_PATH + "d_assassinTest_doesNotFlee", "c_msic_zeroDamage");
+
+        Position mercPos = TestUtils.getEntityById(res, "assassin").getPosition();
+        assertEquals(new Position(1, 5), mercPos);
+
+        res = dmc.tick(Direction.UP);
+
+        mercPos = TestUtils.getEntityById(res, "assassin").getPosition();
+        assertEquals(new Position(1, 4), mercPos);
+
+        res = dmc.tick(Direction.UP);
+
+        mercPos = TestUtils.getEntityById(res, "assassin").getPosition();
+        assertEquals(new Position(1, 3), mercPos);
+    }
+}
