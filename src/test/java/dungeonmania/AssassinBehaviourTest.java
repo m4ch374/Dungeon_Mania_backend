e0f6452ab2de1c3351efdd5953ba6170b12232a5
Henry Wan <z5338828@ad.unsw.edu.au>
2022-07-29 05:24:55 +1000

    Test: removed irrelevent test

--- src/test/java/dungeonmania/AssassinBehaviourTest.java
+++ src/test/java/dungeonmania/AssassinBehaviourTest.java
@@ -32,25 +32,4 @@ public class AssassinBehaviourTest {
         mercPos = TestUtils.getEntityById(res, "assassin").getPosition();
         assertEquals(new Position(1, 3), mercPos);
     }
-
-    @Test
-    @DisplayName("Test Assassin does not flee")
-    public void testMovement_doesNotFlee() {
-        DungeonManiaController dmc = new DungeonManiaController();
-
-        DungeonResponse res = dmc.newGame(DIR_PATH + "d_assassinTest_doesNotFlee", "c_msic_zeroDamage");
-
-        Position mercPos = TestUtils.getEntityById(res, "assassin").getPosition();
-        assertEquals(new Position(1, 5), mercPos);
-
-        res = dmc.tick(Direction.UP);
-
-        mercPos = TestUtils.getEntityById(res, "assassin").getPosition();
-        assertEquals(new Position(1, 4), mercPos);
-
-        res = dmc.tick(Direction.UP);
-
-        mercPos = TestUtils.getEntityById(res, "assassin").getPosition();
-        assertEquals(new Position(1, 3), mercPos);
-    }
 }
