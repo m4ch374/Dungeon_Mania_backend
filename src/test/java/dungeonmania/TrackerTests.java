2cf1738b52f1d50d06597fd3314ec4eb1cf742ae
Henry Wan <z5338828@ad.unsw.edu.au>
2022-07-30 23:56:27 +1000

    Test: modified test

--- src/test/java/dungeonmania/TrackerTests.java
+++ src/test/java/dungeonmania/TrackerTests.java
@@ -60,6 +60,10 @@ public class TrackerTests {
 
         res = dmc.tick(Direction.DOWN);
         assertEquals("", res.getGoals());
+
+        res = dmc.tick(Direction.DOWN);
+        System.out.println(res.getGoals());
+        assertTrue(res.getGoals().contains(":boulders"));
     }
 
     @Test
