569ee2a90352e5bb5f820f14b2f65d0e402829fe
Hanqi Bai <hanqi.bai@student.unsw.edu.au>
2022-07-31 05:23:51 +1000

    tests for logic stuff and fix

--- src/main/java/dungeonmania/DungeonObjects/Entities/LogicEntities/Statics/LightBulb.java
+++ src/main/java/dungeonmania/DungeonObjects/Entities/LogicEntities/Statics/LightBulb.java
@@ -1,13 +1,11 @@
 package dungeonmania.DungeonObjects.Entities.LogicEntities.Statics;
 
+import dungeonmania.DungeonObjects.EntityTypes;
 import dungeonmania.DungeonObjects.Entities.LogicEntities.LogicEntity;
+import dungeonmania.response.models.EntityResponse;
 import dungeonmania.util.DungeonFactory.EntityStruct;
 
 public class LightBulb extends LogicEntity {
-
-    public LightBulb(EntityStruct metaData) {
-        super(metaData);
-    }
 
     public LightBulb(EntityStruct metaData, String logic) {
         super(metaData, logic);
@@ -19,7 +17,10 @@ public class LightBulb extends LogicEntity {
     }
 
     @Override
-    public String getLogic() {
-        return super.getLogic();
+    public EntityResponse toEntityResponse() {
+        if (isActive()) {
+            return new EntityResponse(getId(), EntityTypes.LIGHT_BULB_ON.toString(), getMap().getEntityPos(this), false);
+        }
+        return new EntityResponse(getId(), EntityTypes.LIGHT_BULB_OFF.toString(), getMap().getEntityPos(this), false);
     }
 }
