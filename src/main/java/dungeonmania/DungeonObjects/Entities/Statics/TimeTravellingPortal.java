cfa27f12e40a19ea5e316cd524f1ab4360e27b61
Amith Jacob Kovoor <a.jacobkovoor@student.unsw.edu.au>
2022-07-31 21:06:55 +1000

    amith (uml)-> master

--- src/main/java/dungeonmania/DungeonObjects/Entities/Statics/TimeTravellingPortal.java
+++ src/main/java/dungeonmania/DungeonObjects/Entities/Statics/TimeTravellingPortal.java
@@ -1,14 +1,19 @@
 package dungeonmania.DungeonObjects.Entities.Statics;
 
 import dungeonmania.DungeonObjects.Entities.Entity;
+import dungeonmania.Interfaces.IStaticInteractable;
+import dungeonmania.exceptions.InvalidActionException;
 import dungeonmania.util.DungeonFactory.EntityStruct;
 
 // TODO: stubbed
-public class TimeTravellingPortal extends Entity {
+public class TimeTravellingPortal extends Entity implements IStaticInteractable{
 
     public TimeTravellingPortal(EntityStruct metaData) {
         super(metaData);
         //TODO Auto-generated constructor stub
     }
     
+    
+    @Override
+    public void interactedBy(Entity interactor) throws InvalidActionException {}
 }
